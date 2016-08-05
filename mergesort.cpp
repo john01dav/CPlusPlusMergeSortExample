45df24ce32c830114edac776d18c5e626bba0e17
john01dav <john01dav@gmail.com>
2016-08-05 05:21:08 -0500

    Added comments and improved code style in mergesort function

--- mergesort.cpp
+++ mergesort.cpp
@@ -6,38 +6,37 @@
 using std::vector;
 
 void mergesort(vector<int> &vec){
-	if(vec.size() == 1) return;
-
+	//find midpoints in array
 	vector<int>::size_type midpoint = vec.size() / 2;
 	vector<int>::iterator midpoint_iter = vec.begin() + midpoint;
 
+	//split main array into two subarrays
 	vector<int> vector_a, vector_b;
 
-	for(auto i=vec.begin();i<midpoint_iter;++i){
+	for(auto i=vec.begin();i!=midpoint_iter;++i){
 		vector_a.push_back(*i);
 	}
-	for(auto i=midpoint_iter;i<vec.end();++i){
+	for(auto i=midpoint_iter;i!=vec.end();++i){
 		vector_b.push_back(*i);
 	}
 
-	mergesort(vector_a);
-	mergesort(vector_b);
+	//if needed, sort subarrays
+	if(vector_a.size() > 1) mergesort(vector_a);
+	if(vector_b.size() > 1) mergesort(vector_b);
 
+	//merge now sorted subarrays
 	vec.clear();
 
 	vector<int>::iterator vec_a_pos = vector_a.begin();
 	vector<int>::iterator vec_b_pos = vector_b.begin();
 
-	while(vec_a_pos != vector_a.end() || vec_b_pos != vector_b.end()){
+	while(vec_a_pos != vector_a.end() || vec_b_pos != vector_b.end()){ //I am especially interested in ways to improve this loop
 		if(vec_a_pos == vector_a.end()){
 			vec.push_back(*(vec_b_pos++));
 		}else if(vec_b_pos == vector_b.end()){
 			vec.push_back(*(vec_a_pos++));
-		}else if(*vec_a_pos < *vec_b_pos){
-			vec.push_back(*(vec_a_pos++));
 		}else{
-			vec.push_back(*(vec_b_pos++));
+			vec.push_back(*vec_a_pos < *vec_b_pos ? *(vec_a_pos++) : *(vec_b_pos++));
 		}
 	}
-
 }
